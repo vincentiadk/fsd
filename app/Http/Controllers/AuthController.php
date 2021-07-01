92a1bd364d42736ea4ee1ca74e007979a01a2761
vincentiadk <vincentiadksitanggang@gmail.com>
2021-07-01 07:18:22 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use App\Models\Log;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -18,35 +18,41 @@ class AuthController extends Controller
                 $password = request('password');
                 $fieldType = filter_var(request('username'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
                 $query = User::where("$fieldType", $username)->whereNotNull('email_verified_at');
-                
+
                 if ($query->count() > 0) {
-                    if (Hash::check($password, $query->first()->password)) {
-                        $user = $query->first();
-                        $time = now();
-                        session([
-                            'id' => $user->id,
-                            'username' => $user->username,
-                            'role_id' => $user->role_id,
-                            'email' => $user->email,
-                            'name'  => $user->name,
-                            'last_login' => $time
-                        ]);
-                        $user->update([
-                            'last_login'=> $time
-                        ]);
-                        Log::create([
-                            'user_id'   => session('id'),
-                            'activity'  => 'login'
-                        ]);
-                        return redirect('admin/dashboard');
-                    } else {
+                    if ($query->first()->enable == 0) {
                         return redirect()->back()->with([
-                            'failed' => 'Maaf, username atau password tidak sesuai.',
+                            'failed' => 'Maaf, akun Anda tidak aktif.',
                         ]);
+                    } else {
+                        if (Hash::check($password, $query->first()->password)) {
+                            $user = $query->first();
+                            $time = now();
+                            session([
+                                'id' => $user->id,
+                                'username' => $user->username,
+                                'role_id' => $user->role_id,
+                                'email' => $user->email,
+                                'name' => $user->name,
+                                'last_login' => $time,
+                            ]);
+                            $user->update([
+                                'last_login' => $time,
+                            ]);
+                            Log::create([
+                                'user_id' => session('id'),
+                                'activity' => 'login',
+                            ]);
+                            return redirect('admin/dashboard');
+                        } else {
+                            return redirect()->back()->with([
+                                'failed' => 'Maaf, password tidak sesuai.',
+                            ]);
+                        }
                     }
                 } else {
                     return redirect()->back()->with([
-                        'failed' => 'Maaf, username atau password Anda salah',
+                        'failed' => 'Maaf, username/email Anda tidak ditemukan',
                     ]);
                 }
             } else {
@@ -58,8 +64,8 @@ class AuthController extends Controller
     public function logout()
     {
         Log::create([
-            'user_id'   => session('id'),
-            'activity'  => 'logout'
+            'user_id' => session('id'),
+            'activity' => 'logout',
         ]);
         session()->flush();
         return redirect('/login');
